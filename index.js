8743738c7d13dcef1354d01de8488a8a0a385019
Kelsea Fields <kelsea.fields@yahoo.com>
2020-11-25 12:10:29 -0800

    adding to my add employee function

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+const cTable = require('console.table');
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -13,7 +14,7 @@ var connection = mysql.createConnection({
   // Your password
   password: "KFields@1991!",
 //   Don't Forget to input DATABASE NAME HERE
-  database: ""
+  database: "employee_trackerDB"
 });
 
 connection.connect(function(err) {
@@ -82,17 +83,67 @@ connection.connect(function(err) {
   }
 //   creating functions for cases
 
-  function employeeView() {}
+  function employeeView() {
+    var query = "SELECT * FROM employee;";
+    connection.query(query, function(err, res) {
+        if (err) throw err;
+      console.table(res)
+      start();
+    })
+  }
 //   should render all employees list and info
 
-  function addEmployee() {}
+  function addEmployee() {
+    inquirer
+    .prompt({
+      name: "firstname",
+      type: "input",
+      message: "Employee's first name?"
+    },
+    {
+        name: "lastname",
+        type: "input",
+        message: "Employee's last name?"
+    },
+    {
+        name: "role",
+        type: "rawlist",
+        message: "Which Role would you like to assign?",
+        choices: [
+          "Sales Lead",
+          "Sales Person",
+          "Lead Engineer",
+          "Software Engineer",
+          "Accountant",
+          "Legal Team Lead",
+          "Lawyer"
+        ]
+    },
+    {
+        name: "manager",
+        type: "rawlist",
+        message: "Which Role would you like to assign?",
+        choices: [
+          ""
+        //   how do I get manager information here so that I can use info as a choice?
+        ]
+      }
+
+    ).then(function(answer) {
+    connection.query("INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?)", [answer.firstname, answer.lastname, answer.role], function(err, res) {
+            if (err) throw err;
+          console.table(res)
+          start();
+    })
+    })
+}
 //  should add employee to list of employees
     // input: firstname
     // input: lastname
     // list option for: title/role
     // list option for: department
-    //nput:  salary
-    // ist option for: manager
+    //input:  salary
+    // list option for: manager
 
   function employeesByDepartment() {}
 //   should render a chart of employees based off of which department they're in
